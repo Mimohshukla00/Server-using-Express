fc7506405370975c29f8d7d8c3d5770c1455574b
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-04-08 22:01:57 +0530

    comit

--- server.js
+++ server.js
@@ -36,4 +36,5 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/CarDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}).then(()=>{console.log('database connected')})
+.catch((err)=>console.log("error"));
